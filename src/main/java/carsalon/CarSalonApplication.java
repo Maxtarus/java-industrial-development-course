03b90b61a955252c02a9eefd3f734f08c1d9db66
Maxtarus <m4xstarstarostin@yandex.ru>
2023-10-13 11:23:11 +0300

    add HomeController and CarSalonApplication classes and test for the first one

---
+++ src/main/java/carsalon/CarSalonApplication.java
@@ -0,0 +1,11 @@
+package carsalon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSalonApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CarSalonApplication.class, args);
+	}
+}
---
+++ src/main/java/carsalon/controller/HomeController.java
@@ -0,0 +1,15 @@
+package carsalon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/car_sale")
+public class HomeController {
+
+    @GetMapping
+    public String getHomePage() {
+        return "home";
+    }
+}
